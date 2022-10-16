91e19cfb9ff24ed19efdbc82b6fb3868d57cf57b
prosjl <test@naver.com>
2022-10-17 01:56:20 +0900

    feat : 송금기능 추가 및 로그인 기능 추가
    [project15-S01]
    1. test DB연결
    2. 송금 API 생성 및 송금
    3. 니모닉 로그인
    4. passwd 로그인

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express');
-// const session = require('express-session');
-// const passport = require('passport');
-
-
 const logger = require('./config/winston.js');
-//const login = require('./routes/login.js');
-//const join = require('./routes/join.js');
 const test = require('./routes/test')
+const db = require('./lib/db.js');
 
 
 
@@ -15,25 +10,14 @@ const app = express();
 //app.use('/login', login);
 //app.use('/join', join);
 app.use('/test', test);
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(flash());
 
 
 app.listen(3000, function(){
-    logger.info('[app.js] Connected, 3000 port')
+    logger.info('[DASHCASH] Connected, 3000 port')
 
-    // db.connectDB().then(conn => {
-    //     conn.release()
-    //     logger.info(conn)
-    //     conn.query( constants.CHANGE_DEADLINE, function (err, result) {
-    //     if(err) {
-    //         throw err;
-    //     }
-    //     logger.info("deadline update!");
-    //     });
-    // }).catch(err => {
-    //     conn.release()
-    //     logger.error(err)
-    // })
-   //process.send('ready')
 });
 
 
